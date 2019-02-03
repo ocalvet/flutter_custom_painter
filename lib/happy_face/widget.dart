7d700a719172dda80b03ca3746250c280bb820ec
Ovidio R. Calvet <calveto@gmail.com>
2019-02-03 09:17:03 -0500

    Update widget to take colors

--- lib/happy_face/widget.dart
+++ lib/happy_face/widget.dart
@@ -3,13 +3,18 @@ import 'package:flutter_custom_painter/happy_face/drawing.dart';
 
 class HappyFace extends StatelessWidget {
   final double size;
+  final Color fillColor;
+  final Color strokeColor;
 
-  const HappyFace({Key key, this.size}) : super(key: key);
+  const HappyFace({Key key, this.size, this.fillColor, this.strokeColor}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return CustomPaint(
-      foregroundPainter: Drawing(),
+      foregroundPainter: Drawing(
+        fillColor: fillColor,
+        strokeColor: strokeColor,
+      ),
       child: Container(
         width: size,
         height: size,
