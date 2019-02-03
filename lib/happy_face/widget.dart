778f30fdee2ff557b201f7cb9ddc9dcf6d3b2d26
Ovidio R. Calvet <calveto@gmail.com>
2019-02-03 09:12:28 -0500

    Add size as a property of the happy face widget

--- lib/happy_face/widget.dart
+++ lib/happy_face/widget.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_custom_painter/happy_face/drawing.dart';
 
 class HappyFace extends StatelessWidget {
+  final double size;
+
+  const HappyFace({Key key, this.size}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return CustomPaint(
-      // size: Size(200, 200),
       foregroundPainter: Drawing(),
       child: Container(
-        // child: Text('Home'),
-        width: 200,
-        height: 100,
-        // color: Colors.amber,
+        width: size,
+        height: size,
       ),
     );
   }
