7c54f724b65c5444e79c842bf5eae9a39654d536
Ovidio R. Calvet <calveto@gmail.com>
2019-02-06 03:15:03 -0500

    Move paints creation to constructor

--- lib/happy_face/drawing.dart
+++ lib/happy_face/drawing.dart
@@ -4,32 +4,38 @@ import 'package:flutter/rendering.dart';
 class Drawing extends CustomPainter {
   final Color fillColor;
   final Color strokeColor;
+  final Paint facePaint;
+  final Paint grayStroke;
 
-  Drawing({this.fillColor, this.strokeColor});
+  Drawing({this.fillColor, this.strokeColor})
+      : facePaint = Paint()
+          ..strokeWidth = 4.0
+          ..style = PaintingStyle.fill
+          ..color = fillColor ?? Colors.green,
+        grayStroke = Paint()
+          ..strokeWidth = 2.0
+          ..style = PaintingStyle.stroke
+          ..color = strokeColor ?? Colors.black38;
+
   @override
   void paint(Canvas canvas, Size size) {
-    double centerX = size.width/2;
-    double centerY = size.height/2;
+    double centerX = size.width / 2;
+    double centerY = size.height / 2;
     double length = size.shortestSide;
-    double faceRadius = length/2;
-    double eyeDistanceFromCenter = length/4.5;
+    double faceRadius = length / 2;
+    double eyeDistanceFromCenter = length / 4.5;
     double leftEyeCenterX = centerX - eyeDistanceFromCenter;
     double rightEyeCenterX = centerX + eyeDistanceFromCenter;
-    double eyeCenterY = centerY - length/6;
-    double eyeRadius = length/8;
-    Paint facePaint = Paint()
-    ..strokeWidth = 4.0
-    ..style = PaintingStyle.fill
-    ..color = fillColor ?? Colors.green;
-    Paint grayStroke = Paint()
-    ..strokeWidth = 2.0
-    ..style = PaintingStyle.stroke
-    ..color = strokeColor ?? Colors.black38;
-    Rect mouthArea = Rect.fromCircle(center: Offset(centerX, centerY), radius: faceRadius - faceRadius / 4);
+    double eyeCenterY = centerY - length / 6;
+    double eyeRadius = length / 8;
+    Rect mouthArea = Rect.fromCircle(
+        center: Offset(centerX, centerY), radius: faceRadius - faceRadius / 4);
     canvas.drawCircle(Offset(centerX, centerY), faceRadius, facePaint);
     canvas.drawCircle(Offset(centerX, centerY), faceRadius, grayStroke);
-    canvas.drawCircle(Offset(leftEyeCenterX, eyeCenterY), eyeRadius, grayStroke);
-    canvas.drawCircle(Offset(rightEyeCenterX, eyeCenterY), eyeRadius, grayStroke);
+    canvas.drawCircle(
+        Offset(leftEyeCenterX, eyeCenterY), eyeRadius, grayStroke);
+    canvas.drawCircle(
+        Offset(rightEyeCenterX, eyeCenterY), eyeRadius, grayStroke);
     canvas.drawArc(mouthArea, 0.5, 2, false, grayStroke);
   }
 
@@ -37,5 +43,4 @@ class Drawing extends CustomPainter {
   bool shouldRepaint(CustomPainter oldDelegate) {
     return false;
   }
-
 }
