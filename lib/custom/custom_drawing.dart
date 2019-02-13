063bf74d58512235dd5f6244388b5743a783ad50
Ovidio R. Calvet <calveto@gmail.com>
2019-02-13 15:39:47 -0500

    Remove line

--- lib/custom/custom_drawing.dart
+++ lib/custom/custom_drawing.dart
@@ -16,16 +16,12 @@ class CustomDrawing extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    double centerX = size.width / 2;
-    double centerY = size.height / 2;
-    double length = size.shortestSide;
-    canvas.drawLine(Offset(10, 10), Offset(size.width - 10, size.height - 10), grayStroke);
     Path path = Path()
       ..moveTo(30, 10)
       ..lineTo(170, 90)
       ..lineTo(190, 60)
       ..close();
-    canvas.drawShadow(path, Colors.black, 2.0, false);
+    canvas.drawShadow(path, Colors.black, 5.0, false);
     canvas.drawPath(path, fillColor);
   }
 
